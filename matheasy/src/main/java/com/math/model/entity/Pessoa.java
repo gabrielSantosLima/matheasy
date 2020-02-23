c32eec883113871d396ae4ff4bce0b520e2432d1
gabrielLimaIfam <49927177+gabrielLimaIfam@users.noreply.github.com>
2020-02-23 17:25:03 -0400

    Primeiro commit

---
+++ matheasy/src/main/java/com/math/model/entity/Pessoa.java
@@ -0,0 +1,5 @@
+package com.math.model.entity;
+
+public class Pessoa {
+	
+}
---
+++ matheasy/src/main/java/com/principal/matheasy/MatheasyApplication.java
@@ -0,0 +1,13 @@
+package com.principal.matheasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatheasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatheasyApplication.class, args);
+	}
+
+}
